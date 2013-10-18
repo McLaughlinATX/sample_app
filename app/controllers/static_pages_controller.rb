eab89713fde0600606aa83546361067cd238d8b1
Mike McLaughlin <mike_atm_97@yahoo.com>
2013-10-17 19:06:25 -0500

    Add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  
+  # "action" home
+  # 	it has a corresponding "view" called home.html.erb
+  #     located at: app/views/static_pages/home.html.erb
+  def home 			
+  	
+
+  end
+
+  # "action" help
+  # 	it has a corresponding "view" called help.html.erb
+  #     located at: app/views/static_pages/help.html.erb
+  def help
+  end
+end
