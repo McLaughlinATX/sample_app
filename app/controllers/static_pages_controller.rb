38f8d290470c5368563c6f5ac8fb5d3da0f61c0b
Mike McLaughlin <mike_atm_97@yahoo.com>
2013-10-22 10:57:01 -0500

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,27 +3,32 @@ class StaticPagesController < ApplicationController
   # "action" home
   # 	it has a corresponding "view" called home.html.erb
   #     located at: app/views/static_pages/home.html.erb
-  def home 			
-  	
+  def home 			  	
 
   end
+
 
   # "action" help
   # 	it has a corresponding "view" called help.html.erb
   #     located at: app/views/static_pages/help.html.erb
   def help
+
+
   end
 
   # "action" about
   #   it has a corresponding "view" called about.html.erb
   #     located at: app/views/static_pages/about.html.erb
   def about
+
+
   end
 
   # "action" contact
   #   it has a corresponding "view" called contact.html.erb
   #     located at: app/views/static_pages/contact.html.erb
   def contact
+
   end
 
 
